87dae198fe5d563d591127f59df850f51e0cbf3d
Aditya <adipusk@gmail.com>
2024-09-26 16:31:27 +0530

    Data Ingestion setup done

--- Reddit_airflow_pipeline/reddit_etl.py
+++ Reddit_airflow_pipeline/reddit_etl.py
@@ -3,13 +3,23 @@ import json
 import praw
 from datetime import datetime
 import s3fs
+from env import CLIENT_SECRET, CLIENT_ID, USERAGENT
+
 
 reddit = praw.Reddit(
-    client_id="CLIENT_ID",
-    client_secret="CLIENT_SECRET",
-    password="PASSWORD",
-    user_agent="USERAGENT",
-    username="USERNAME",
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    user_agent=USERAGENT,
 )
+posts = []
+for submission in reddit.subreddit('developers').hot(limit=100):
+    post = {
+        "title": submission.title,
+        "author": submission.author,
+        "score": submission.score
+    }
+    posts.append(post)
 
-print(reddit.read_only)
+
+df = pd.DataFrame(posts)
+df.to_csv("reddit_posts")
